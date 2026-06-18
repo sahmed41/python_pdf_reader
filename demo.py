a9fcbeb27ec33cd1b8d75b9e9013571597ba756a
sahmed <s.ahmed.the1st@gmail.com>
2026-06-18 12:42:35 +0200

    Daily Commit

--- demo.py
+++ demo.py
@@ -5,6 +5,21 @@ import _modules.pdf_engine as pe
 
 filePath = Path("test_environment/files/Story_1.pdf")
 file = pdf.open(filePath)
+file2 = pdf.open()
+file2.insert_pdf(file, to_page=2)
+file2.save("test_environment/files/Story_Z.pdf")
 
 
-pe.print_pdf(file)
+# pe.print_pdf(file)
+print(file.metadata)
+
+
+file2.close()
+
+
+filePic = file.load_page(1)
+filePic = filePic.get_pixmap()
+filePic.save("test_environment/files/Story_1_0_img.png")
+# Returns the raw image data as PNG bytes
+# image_bytes = filePic.tobytes("png")
+file.close()
